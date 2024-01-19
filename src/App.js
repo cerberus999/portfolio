8be01023e72400907bac0cbfef5bfe31823be7e9
cerberus999 <cerverus97@Cerberus97>
2024-01-19 18:04:35 -0400

    19012024, inicio

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+import './App.css';
+import NavBar from './NavBar'
+
+function App() {
+  return (
+    <div className="App">
+      <NavBar/>
+      <p>Hello world</p>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/NavBar.js
@@ -0,0 +1,6 @@
+export default function NarBar() {
+  return (
+  <navbar>
+  </navbar>
+  );
+}
