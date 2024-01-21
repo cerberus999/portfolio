a1c1d2875de89fe7372478ea8f17b0bef592e80b
cerberus999 <cerverus97@Cerberus97>
2024-01-20 20:06:45 -0400

    200124

--- src/NavBar.js
+++ src/NavBar.js
@@ -3,7 +3,14 @@ import './NavBar.css'
 export default function NavBar() {
   return (
   <div className="navbar">
-    
+    <div className="barright">
+      <div className="rounded-md">
+        <a href="#aboutme">About Me</a>
+      </div>
+      <div className="rounded-md">
+        <a href="#projects">Projects</a>
+      </div>
+    </div>
   </div>
   );
 }
